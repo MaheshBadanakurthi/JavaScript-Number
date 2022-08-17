6b8ba85795c3e44185e36b5ba94965e171d51b67
Mahesh <itstribe.mahi@gmail.com>
2022-08-17 15:23:18 +0530

    Number methods

---
+++ number.js
@@ -0,0 +1,48 @@
+// let x="7"+3;  //one is string and number
+//  console.log('I added 7 to 3 by using string nad number',x); 
+
+// let x='45'/3;
+// console.log(x)
+
+
+// let x=123;
+// let y='orange';
+// console.log(x+y);
+
+// let x=23-'orange'
+// console.log(x);  //it gives NaN
+
+//  let x= isNaN(4-"4");
+//  console.log(x);
+//  console.log(typeof(x));
+
+// let x=1234567890;
+// console.log(x+=3);
+// console.log(typeof(x));
+
+// let x=0.1+0.2;
+// console.log(x);
+// console.log(x.toFixed(3));
+
+// let x= 99999999999;
+// console.log(x);
+
+// let x=123;
+// let y= new Number(2345);
+// console.log(typeof(x));
+// console.log(typeof(y));
+
+// number ,methods
+let x=2345;
+let y=2345.765;
+console.log(Number.isNaN(x));
+console.log(Number.isFinite(x));
+console.log(Number.isInteger(y));
+console.log(Number.parseFloat(x));
+console.log(x.toFixed(2));
+
+
+// //max and min methods
+// let x=Number.MAX_VALUE;
+// console.log(x);
+
